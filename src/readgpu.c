716830e6a748d1564d3098214928a18404ad8219
skaura <skaura@qq.com>
2024-03-16 18:00:13 +0800

    first commit

---
+++ src/readgpu.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+
+#define  GPU_MAX_FREP "/sys/devices/platform/ff9a0000.gpu/devfreq/ff9a0000.gpu/max_freq"
+#define  GPU_MIN_FREP "/sys/devices/platform/ff9a0000.gpu/devfreq/ff9a0000.gpu/min_freq"
+#define  GPU_LOAD "/sys/devices/platform/ff9a0000.gpu/devfreq/ff9a0000.gpu/load"
+
+int main()
+{
+ FILE *fp;
+unsigned long frep;
+char buff[30];
+int ch = 0;
+
+
+if((fp = fopen(GPU_MAX_FREP,"r")) == NULL)
+{
+    printf("failure to open file\n");
+    exit(EXIT_FAILURE);
+}
+
+if((fread(&buff,sizeof(buff),1,fp)) > 0)
+{
+    frep = (unsigned long)buff;
+    printf("%lu\n",frep);
+}
+// while ((ch = getc(fp)) != EOF)
+// {
+//     putchar(fp);
+// }
+fclose(fp);
+return 0;
+
+}
