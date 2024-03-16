06a4491ef330bf848988ca25c7659ac287c78f82
skaura <skaura@qq.com>
2024-03-16 18:58:11 +0800

    1

---
+++ include/gpu_read.h
@@ -0,0 +1,15 @@
+#ifndef _GPU_READ_H
+#define _GPU_READ_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
+unsigned long read_gpu_frep_load(FILE *fp,const char *path);
+
+
+
+
+
+
+
+#endif
--- src/readgpu.c
+++ src/readgpu.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
-
+#include "gpu_read.h"
 
 #define  GPU_MAX_FREP "/sys/devices/platform/ff9a0000.gpu/devfreq/ff9a0000.gpu/max_freq"  // 800000000
 #define  GPU_MIN_FREP "/sys/devices/platform/ff9a0000.gpu/devfreq/ff9a0000.gpu/min_freq"   // 200000000
@@ -10,26 +10,26 @@
 int main()
 {
 FILE *fp;
-unsigned long frep;
-char a[8];
+unsigned long max_frep,min_frep, load;
+// char a[8];
 
 
-if((fp = fopen(GPU_MAX_FREP,"r")) == NULL)
-{
-    printf("failure to open file\n");
-    exit(EXIT_FAILURE);
-}
-
-fread(&a,sizeof(a),8,fp);
-
-// frep = (unsigned long)buff;
-frep = atol(a);
-printf("%lu\n",frep);
-
-// while ((ch = getc(fp)) != EOF)
+// if((fp = fopen(GPU_MAX_FREP,"r")) == NULL)
 // {
-//     putchar(fp);
+//     printf("failure to open file\n");
+//     exit(EXIT_FAILURE);
 // }
+
+// fread(&a,sizeof(a),8,fp);
+
+// // frep = (unsigned long)buff;
+// frep = atol(a);
+// printf("%lu\n",frep);
+
+// // while ((ch = getc(fp)) != EOF)
+// // {
+// //     putchar(fp);
+// // }
 fclose(fp);
 return 0;
 
