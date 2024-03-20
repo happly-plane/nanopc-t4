d5fc6b52bf804351e49b091004c2142a976a81f3
skaura <skaura@qq.com>
2024-03-20 12:49:57 +0800

    1

--- src/readgpu.c
+++ src/readgpu.c
@@ -71,9 +71,8 @@ while (1)
         // OLED_ShowStr(0, 3, (unsigned char *)usage, 1);  //测试6*8字符
         snprintf(str, 5, "%.2f%%", usage); // 将浮点数转换为字符串
      // OLED_ShowStr(0, 4, (unsigned char *)"Hello wildfire", 2); //测试8*16字符
-        OLED_ShowStr(0, 1, (unsigned char *)str);  //测试6*8字符
+        OLED_ShowStr(2, 1, (unsigned char *)str);  //测试6*8字符
         sleep(4);
-        OLED_CLS(); //清屏
 }
 
     return 0;
